2657c917fd7417a2cd8724fd8d4061a4909b3cb0
serbiow <sergiohenriqueqqq@gmail.com>
2024-04-05 11:11:54 -0300

    funcoes editar

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const express = require("express")
 const app = express()
 const handlebars = require("express-handlebars").engine
+const bodyParser = require("body-parser")
+const post = require("./models/post")
 
 app.engine("handlebars", handlebars({defaultLayout: "main"}))
-app.set('view engine', 'handlebars')
+app.set("view engine", "handlebars")
 
-app.get("/", (req, res) => {
-    res.render("index")
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get("/", function(req, res){
+    res.render("cadastrar")
 })
 
-app.get("/atualizar", (req, res) => {
-    res.render("atualizar")
+app.post("/cadastrar", function(req, res){
+    post.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(function(){
+        res.redirect("/")
+    }).catch(function(erro){
+        res.send("Falha ao cadastrar os dados: " + erro)
+    })
 })
 
-app.get("/consultar", (req, res) => {
-    res.render("consultar")
+app.get("/consulta", function(req, res){
+    post.findAll().then(function(posts){
+        res.render("consulta", {post: posts})
+    })
 })
 
-app.listen(8081, () => {
-    console.log("Servidor ativo")
+app.get("/editar/:id", function(req, res){
+    post.findAll({where: {"id":req.params.id}}).then(function(posts){
+        res.render("editar", {post: posts})
+    })
+})
+
+app.listen(8081, function(){
+    console.log("Servidor Ativo!")
 })
